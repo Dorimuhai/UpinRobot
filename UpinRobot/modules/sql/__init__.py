7a063dd344f79eada8022af8754731b857b1055f
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-02 10:45:06 +0700

    Update __init__.py

--- UpinRobot/modules/sql/__init__.py
+++ UpinRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from VegetaRobot import DB_URI
+from UpinRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
