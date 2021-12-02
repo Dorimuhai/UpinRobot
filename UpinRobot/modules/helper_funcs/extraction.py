ccdd5510bcf636a6a933cae0a9e25b3c85d01420
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-02 10:31:07 +0700

    Update extraction.py

--- UpinRobot/modules/helper_funcs/extraction.py
+++ UpinRobot/modules/helper_funcs/extraction.py
@@ -1,7 +1,7 @@
 from typing import List, Optional
 
-from VegetaRobot import LOGGER
-from VegetaRobot.modules.users import get_user_id
+from UpinRobot import LOGGER
+from UpinRobot.modules.users import get_user_id
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
