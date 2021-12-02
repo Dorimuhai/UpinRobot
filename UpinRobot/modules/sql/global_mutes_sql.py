5760983f6f46d2127994c808b32955aa10600bd3
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-02 10:58:27 +0700

    Update global_mutes_sql.py

--- UpinRobot/modules/sql/global_mutes_sql.py
+++ UpinRobot/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from VegetaRobot.modules.sql import BASE, SESSION
+from UpinRobot.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
