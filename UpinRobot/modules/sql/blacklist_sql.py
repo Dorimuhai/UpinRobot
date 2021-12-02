8811ccd675170b294bfaaa942c7a42131d924f5e
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-02 10:47:25 +0700

    Update blacklist_sql.py

--- UpinRobot/modules/sql/blacklist_sql.py
+++ UpinRobot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from VegetaRobot.modules.sql import SESSION, BASE
+from UpinRobot.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
