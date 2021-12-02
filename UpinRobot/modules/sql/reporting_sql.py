4c8412834be1d5311f4098dea49a486b9d68c9fa
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-02 11:00:21 +0700

    Update reporting_sql.py

--- UpinRobot/modules/sql/reporting_sql.py
+++ UpinRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from VegetaRobot.modules.sql import BASE, SESSION
+from UpinRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
