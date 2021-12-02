5bc6784d6daa6ac94b140e63393e07dfb5b27b59
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-02 10:53:40 +0700

    Update blsticker_sql.py

--- UpinRobot/modules/sql/blsticker_sql.py
+++ UpinRobot/modules/sql/blsticker_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from VegetaRobot.modules.sql import BASE, SESSION
+from UpinRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, Integer, String, UnicodeText, distinct, func
 
 
