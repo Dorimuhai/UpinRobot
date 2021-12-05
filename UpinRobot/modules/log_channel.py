6105b916b70c22efabca3c3fd2ba1e962aad3214
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-05 23:47:26 +0700

    Update log_channel.py

--- UpinRobot/modules/log_channel.py
+++ UpinRobot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from VegetaRobot.modules.helper_funcs.misc import is_module_loaded
+from UpinRobot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from VegetaRobot import EVENT_LOGS, LOGGER, dispatcher
-    from VegetaRobot.modules.helper_funcs.chat_status import user_admin
-    from VegetaRobot.modules.sql import log_channel_sql as sql
+    from UpinRobot import EVENT_LOGS, LOGGER, dispatcher
+    from UpinRobot.modules.helper_funcs.chat_status import user_admin
+    from UpinRobot.modules.sql import log_channel_sql as sql
 
     def loggable(func):
 
@@ -197,7 +197,7 @@ Setting the log channel is done by:
 • forwarding the `/setlog` to the group
 """
 
-    __mod_name__ = "📬Logs"
+    __mod_name__ = "Logs"
 
     LOG_HANDLER = CommandHandler("logchannel", logging)
     SET_LOG_HANDLER = CommandHandler("setlog", setlog)
