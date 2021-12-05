e4602de3066eb514edf9577bd95a519358752eb3
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-05 20:26:47 +0700

    Update google.py

--- UpinRobot/modules/google.py
+++ UpinRobot/modules/google.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib
-from VegetaRobot import telethn as tbot
+from UpinRobot import telethn as tbot
 import glob
 import io
 import os
@@ -21,9 +21,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from VegetaRobot import *
+from UpinRobot import *
 
-from VegetaRobot.events import register
+from UpinRobot.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
@@ -275,7 +275,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "🎒Tools"
+__mod_name__ = "Tools"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
