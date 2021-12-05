074b5179f2a235816619676d04cde24571bf9e3c
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-06 00:33:08 +0700

    Update wiki.py

--- UpinRobot/modules/wiki.py
+++ UpinRobot/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from VegetaRobot import dispatcher
-from VegetaRobot.modules.disable import DisableAbleCommandHandler
+from UpinRobot import dispatcher
+from UpinRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
