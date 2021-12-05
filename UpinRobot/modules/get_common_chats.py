ec92ea1e2a37541e424807e3d6fd45e34e4e2913
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-05 20:17:50 +0700

    Update get_common_chats.py

--- UpinRobot/modules/get_common_chats.py
+++ UpinRobot/modules/get_common_chats.py
@@ -1,9 +1,9 @@
 import os
 from time import sleep
 
-from VegetaRobot import OWNER_ID, dispatcher
-from VegetaRobot.modules.helper_funcs.extraction import extract_user
-from VegetaRobot.modules.sql.users_sql import get_user_com_chats
+from UpinRobot import OWNER_ID, dispatcher
+from UpinRobot.modules.helper_funcs.extraction import extract_user
+from UpinRobot.modules.sql.users_sql import get_user_com_chats
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters
