855d4991f3aa4b64d89aa094736b8abf074b81ab
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-06 00:05:57 +0700

    Update shell.py

--- UpinRobot/modules/shell.py
+++ UpinRobot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from VegetaRobot import LOGGER, dispatcher
-from VegetaRobot.modules.helper_funcs.chat_status import dev_plus
+from UpinRobot import LOGGER, dispatcher
+from UpinRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
