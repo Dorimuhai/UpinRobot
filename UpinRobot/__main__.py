fbf3bc87d7db0cd30a928c7a2da7e71f06d8b159
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-07 07:39:57 +0700

    Update __main__.py

--- UpinRobot/__main__.py
+++ UpinRobot/__main__.py
@@ -327,7 +327,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "Powered by @UnitedSupport\nHere is the help for the *{}* module:\n".format(
+                "Powered by @IpintpiRobot\nHere is the help for the *{}* module:\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
@@ -337,8 +337,8 @@ def help_button(update, context):
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton(text="⬅ Back", callback_data="help_back"),
-                      InlineKeyboardButton(text="⬅ Home", callback_data="upin_back")]]
+                    [[InlineKeyboardButton(text="Go Back", callback_data="help_back"),
+                      InlineKeyboardButton(text="Go Home", callback_data="upin_back")]]
                 ),
             )
 
