ad6915da8915e9d72005b7cf78558fbf0d368e93
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-07 07:20:21 +0700

    Update __init__.py

--- UpinRobot/__init__.py
+++ UpinRobot/__init__.py
@@ -201,7 +201,7 @@ aiohttpsession = ClientSession()
 arq = ARQ("https://thearq.tech", "YIECCC-NAJARO-OLLREW-SJSRIP-ARQ", aiohttpsession)
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("KURUMIBOT", API_ID, API_HASH)
-pgram = Client("VegetaRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+pgram = Client("UpinRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
