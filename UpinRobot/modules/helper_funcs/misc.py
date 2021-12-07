2ff7593f8b9abf213e2cfc9a92b0122ccd18c5b1
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-07 07:25:58 +0700

    Update misc.py

--- UpinRobot/modules/helper_funcs/misc.py
+++ UpinRobot/modules/helper_funcs/misc.py
@@ -71,7 +71,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("⬅ Back to Home ➡", callback_data="upin_back")]]
+        pairs += [[EqInlineKeyboardButton("Back to Home", callback_data="upin_back")]]
 
     return pairs
 
