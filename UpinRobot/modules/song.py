062642d4375f4cd61da791be6bc49e34bcbd1000
Muhammad Nawawi <93711548+Soedirmand@users.noreply.github.com>
2021-12-07 23:17:24 +0700

    Update song.py

--- UpinRobot/modules/song.py
+++ UpinRobot/modules/song.py
@@ -55,7 +55,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n **Made by: @VegetaRobot** '
+        rep = f'🎤 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n **Made by: @IpintpiRobot** '
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -63,7 +63,7 @@ def song(client, message):
         message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit('An error Occured! \nReport at @PegasusSupportOfficial')
+        m.edit('An error Occured! \nReport at @allbefin')
         print(e)
 
     try:
