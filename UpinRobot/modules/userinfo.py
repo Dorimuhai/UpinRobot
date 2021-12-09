7545485af81055f40e78f1864fc2b9651ef3ec15
Muhammad Nawawi El jum'at <93711548+Soedirmand@users.noreply.github.com>
2021-12-09 14:33:34 +0700

    Update userinfo.py

--- UpinRobot/modules/userinfo.py
+++ UpinRobot/modules/userinfo.py
@@ -224,9 +224,9 @@ def info(update: Update, context: CallbackContext):
         return
 
     rep = message.reply_text(
-        "<code>Connecting to pegasus</code>", parse_mode=ParseMode.HTML)
+        "<code>Connecting to ipinxteam</code>", parse_mode=ParseMode.HTML)
 
-    text = (f"╒═══「<b> Pegasus DB Userinfo:</b> 」\n"
+    text = (f"╒═══「<b> UPIN IPIN XTEAM Userinfo:</b> 」\n"
             f"ID: <code>{user.id}</code>\n"
             f"First Name: {html.escape(user.first_name)}")
 
